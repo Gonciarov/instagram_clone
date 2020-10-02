1793b90f17a3100f17bbd4260ee3c8884224aabf
gonciarov <gonciarov@yandex.ru>
2020-10-02 16:29:18 +0100

    user profile finished

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,4 +4,18 @@ class Account < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+  mount_uploader :image, ImageUploader
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def total_followers
+    0
+  end
+
+  def total_following
+    0
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :accounts
   get "/dashboard" => "accounts#index"
+  get "profile/:username" => "accounts#profile", as: :profile
   resources :posts, only: [:new, :create, :show]
   root to: "public#homepage"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
